d8dc5f84835b705f21a022385a6c4bcca37971a9
Randy <39484230+zenith391@users.noreply.github.com>
2019-04-12 14:31:15 +0200

    liburf write working!

--- Users/Shared/Libraries/liburf.lua
+++ Users/Shared/Libraries/liburf.lua
@@ -52,13 +52,16 @@ function lib.newEntry(parent, name, isdir)
 	checkArg(2, name, "string")
 	checkArg(3, isdir, "boolean")
 	local entry = {}
-	if parent == 0 then
+	if parent == nil then
 		entry.id = 0
+		entry.parent = nil
 	else
 		entry.id = getArchive(parent).freeID
 		getArchive(parent).freeID = getArchive(parent).freeID + 1
+		getArchive(parent).entries[entry.id] = entry
+		entry.parent = parent
 	end
-	entry.parent = parent
+	entry.name = name
 	if not isdir then
 		entry.content = ""
 	end
@@ -66,9 +69,23 @@ function lib.newEntry(parent, name, isdir)
 		return isdir
 	end
 	entry.childEntry = function (n, d)
-		return lib.newEntry(entry.id, n, d)
+		return lib.newEntry(entry, n, d)
+	end
+	entry.isEOH = function()
+		return false
 	end
 	return entry
+end
+
+local function eohEntry()
+	local eoh = {}
+	eoh.isDirectory = function()
+		return false
+	end
+	eoh.isEOH = function()
+		return true
+	end
+	return eoh
 end
 
 function lib.writeArchive(arc, s) -- arc = archive, s = stream
@@ -78,6 +95,51 @@ function lib.writeArchive(arc, s) -- arc = archive, s = stream
 	s:write(string.char(arc.version.minor)) -- version minor
 	s:write(string.char(0x12)) -- DC2
 	s:write(string.char(0x0)) -- NULL
+	
+	local ftSize = 0
+	local dataSize = 0
+	for _, v in pairs(arc.entries) do
+		if v.isDirectory() then
+			ftSize = ftSize + 2 + v.name:len()
+		else
+			if v.isEOH() then
+				ftSize = ftSize + 5
+			else
+				ftSize = ftSize + 2 + v.name:len() + 8
+				dataSize = dataSize + v.content:len()
+			end
+		end
+	end
+	
+	local function writeEntry(entry)
+		if entry.isDirectory() then
+			s:write("D")
+			s:write(string.char(entry.name:len()))
+			s:write(entry.name)
+		else
+			if entry.isEOH() then
+				s:write("Z")
+				s:write(u32tostr(dataSize))
+			else
+				s:write("F")
+				s:write(string.char(entry.name:len()))
+				s:write(entry.name)
+				s:write(u32tostr(ftSize + dataSize)) -- offset
+				dataSize = dataSize + entry.content:len()
+				s:write(u32tostr(entry.content:len()))
+			end
+		end
+		s:write(string.char(entry.id))
+		s:write(string.char(entry.parent.id))
+	end
+	for _, v in pairs(arc.entries) do
+		writeEntry(v)
+	end
+	for _, v in pairs(arc.entries) do
+		if not v.isDirectory() and not v.isEOH() then
+			s:write(v.content)
+		end
+	end
 end
 
 function lib.newArchive()
@@ -85,11 +147,13 @@ function lib.newArchive()
 	
 	-- Root
 	arc.freeID = 1
+	arc.entries = {}
 	arc.version = {
 		major=1,
 		minor=0
 	}
-	arc.root = lib.newEntry(0, "", true)
+	arc.root = lib.newEntry(nil, "", true)
+	arc.root.parent = arc
 	
 	arc.isArchive = function()
 		return true
