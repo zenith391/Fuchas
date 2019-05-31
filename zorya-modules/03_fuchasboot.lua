6cc5a872d0a38ada7e851be91136615d4a9ef5ce
Adorable-Catgirl <janeroxanne0@gmail.com>
2019-05-30 19:58:09 -0500

    Add support for Zorya 1.0
    Also adds support for Zorya's OEFI implementation.

--- zorya-modules/03_fuchasboot.lua
+++ zorya-modules/03_fuchasboot.lua
@@ -3,15 +3,23 @@
 local comp = component or require("component")
 local args = {...}
 local envs = args[1]
-for fs in comp.list("filesystem") do
-	if (comp.invoke(fs, "exists", "Fuchas/NT/boot.lua")) then
-		envs.boot[#envs.boot+1] = {"Fuchas NT on "..fs:sub(1, 3), "fuchas", fs, {}}
+local function scan(envs)
+	for fs in comp.list("filesystem") do
+		if (comp.invoke(fs, "exists", "Fuchas/NT/boot.lua")) then
+			envs.boot[#envs.boot+1] = {"Fuchas NT on "..fs:sub(1, 3), "fuchas", fs, {}}
+		end
 	end
+end
+
+if (_ZVER > 0.2) then
+	envs.scan[#envs.scan+1] = scan
+else
+	scan(envs)
 end
 
 envs.hand["fuchas"] = function(fs, args)
 	computer.supportsOEFI = function()
-		return false
+		return _ZVER >= 1.0
 	end
 	computer.supportsZorya = function()
 		return true
@@ -20,5 +28,5 @@ envs.hand["fuchas"] = function(fs, args)
 	loadfile = function(path)
 		return envs.loadfile(fs, path)
 	end
-	envs.loadfile(fs, "Fuchas/NT/boot.lua")(nil, table.unpack(args))
+	envs.loadfile(fs, "Fuchas/NT/boot.lua")(oefi, table.unpack(args))
 end
