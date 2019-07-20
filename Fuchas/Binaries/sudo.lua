21f703bb93f4d672c7a5a173f45eeb0808113d66
Zen1th <39484230+zenith391@users.noreply.github.com>
2019-07-20 11:00:32 +0200

    Update sudo.lua

--- Fuchas/Binaries/sudo.lua
+++ Fuchas/Binaries/sudo.lua
@@ -1,5 +1,10 @@
 -- executes command as "admin" (* permission)
 local security = require("security")
+local shell = require("shell")
 local args = table.pack(...)
 local cmd = args[1]
 table.remove(args, 1)
+if args.n then args.n = args.n - 1 end
+local res = shell.resolve(cmd)
+security.requestPermission("*")
+loadfile(res)(table.unpack(args))
