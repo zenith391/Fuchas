14be61b0aa6f791a1e548a363a9e02d0a8868225
zenith391 <pawalamde@gmail.com>
2020-02-21 22:04:16 +0100

    Improvements
    - Improved "format" program
    - Improved "drive" driver for better use of sector I/O
    - Improved OpenUPT library

--- src/Fuchas/Drivers/drive/drive.lua
+++ src/Fuchas/Drivers/drive/drive.lua
@@ -1,5 +1,5 @@
 local spec = {}
-local SECTOR_IO_TRESHOLD = 400
+local SECTOR_IO_TRESHOLD = 10
 local cp = ...
 
 function spec.getRank()
@@ -34,12 +34,13 @@ function spec.new(address)
 	-- Should be used instdead of drv.readByte when possible, as it *can be* optimized
 	function drv.readBytes(off, len, asString)
 		if off < 1 then error("addresses are 1-based") end
-		local sectorId = math.ceil(off/512)
+		local sectorId = math.ceil((off-1)/512)
+		if sectorId == 0 then sectorId = 1 end
 		if sectorCache.id ~= sectorId then
 			sectorCache.id = sectorId
 			sectorCache.text = drive.readSector(sectorId)
 		end
-		local bytes = table.pack(string.byte(sectorCache.text:sub(off%512, off%512+1+len), 1, len))
+		local bytes = table.pack(string.byte(sectorCache.text:sub((off-1)%512, (off-1)%512+1+len), 1, len))
 		if asString then
 			return sectorCache.text:sub(off%512, off%512+1+len)
 		else
@@ -55,17 +56,16 @@ function spec.new(address)
 				data[i] = string.byte(data[i])
 			end
 		end
-		local sectorId = math.ceil(off/512)
+		local sectorId = math.ceil((off-1)/512)
+		if sectorId == 0 then sectorId = 1 end
 		if sectorCache.id == sectorId then
 			sectorCache.id = -1 -- invalidate cache because we're going to write it
 		end
 		if #data > SECTOR_IO_TRESHOLD then -- if it became more efficient to use sector i/o
-			local sector = math.ceil(off/512)
-			local offset = off%512
-			if offset == 1 then offset = 0 end
-			local sec = drive.readSector(sector)
+			local offset = (off-1)%512
+			local sec = drive.readSector(sectorId)
 			sec = sec:sub(1, offset) .. string.char(table.unpack(data)) .. sec:sub(offset+#data+1)
-			drive.writeSector(sector, sec)
+			drive.writeSector(sectorId, sec)
 		else
 			for i=1, #data do
 				drive.writeByte(off+i-1-1, data[i])
@@ -80,6 +80,10 @@ function spec.new(address)
 
 	function drv.writeByte(off, val)
 		drive.writeByte(off-1, val)
+	end
+
+	function drv.getCapacity()
+		return drive.getCapacity()
 	end
 
 	return drv
--- src/Fuchas/Libraries/openupt.lua
+++ src/Fuchas/Libraries/openupt.lua
@@ -78,6 +78,9 @@ function lib.partitionDriver(driver, partition)
 		end,
 		writeByte = function(addr, value)
 			driver.writeByte(addr+partition.start*512, value)
+		end,
+		getCapacity = function()
+			return (partition.end - partition.start) * 512
 		end
 	}
 end
@@ -126,18 +129,21 @@ function lib.writePartition(driver, partition, progressHandler)
 	addTables(data, string.toByteArray(partition.type))
 	addTables(data, fromTextGUID(partition.guid))
 	addTables(data, string.toByteArray(partition.label:sub(1, 32)))
-	if progressHandler then progressHandler("Writing partition #" .. (partition.id+1)) end
+	if progressHandler then progressHandler("Writing partition #" .. (partition.id+1) .. "..") end
 	driver.writeBytes(off+1, data)
-	--driver.writeBytes(off+1, ("\x00"):rep(64))
+	if progressHandler then progressHandler("Done writing partition.") end
 end
 
 function lib.format(driver, progressHandler)
-	if progressHandler then progressHandler("Writing boot code") end
+	if progressHandler then progressHandler("Writing boot code..") end
 	driver.writeBytes(1, defaultBootCode)
 	for i=0, 7 do
-		lib.writePartition(driver, lib.newPartition(i, 0, 0, "0000000000000000"), progressHandler)
+		--if progressHandler then progressHandler("Erasing partition #" .. (i+1) .. "..") end
+		--driver.writeBytes(25*512+i*64+1, ("\x00"):rep(64))
+		--if progressHandler then progressHandler("Done erasing partition.") end
+		lib.writePartition(driver, lib.newPartition(i), progressHandler)
 	end
-	if progressHandler then progressHandler("Done.") end
+	if progressHandler then progressHandler("Done formatting.") end
 end
 
 return lib
