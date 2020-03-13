add400d132d078961adcae421760bd14868f6851
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2020-03-13 15:33:45 -0400

    Add argument support, clean up require()s

--- Fuchas/Binaries/dir.lua
+++ Fuchas/Binaries/dir.lua
@@ -3,11 +3,12 @@ local shell = require("shell")
 local gpu = require("driver").getDriver("gpu")
 local drive = os.getenv("PWD_DRIVE")
 local pwd = os.getenv("PWD")
-local fullPath = drive .. ":/" .. pwd
 local x = 1
 local vw, vh = gpu.getResolution()
 
 local args, ops = shell.parse(...)
+
+local fullPath = drive .. ":/" .. pwd .. (args[1] and "/" .. args[1]) or ""
 
 local list = filesystem.list(fullPath)
 print("List of " .. fullPath)
@@ -20,7 +21,7 @@ for k, v in list do
 		gpu.setForeground(0x0000FF)
 	end
 	if x + k:len() > vw then
-		require("shell").setCursor(1, require("shell").getY()+1)
+		shell.setCursor(1, shell.getY()+1)
 		x = 0
 	end
 	io.write(k .. " ")
