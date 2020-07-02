92558d3d0379ca7baff5968ee072b1c6c3e82f57
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2020-07-02 13:56:07 -0400

    remove unused things, clean up

--- Fuchas/Kernel/Startup/01_luaapi.lua
+++ Fuchas/Kernel/Startup/01_luaapi.lua
@@ -1,11 +1,4 @@
 -- String
-local _char = string.char
-local _len = string.len
-local _sub = string.sub
-local _reserve = string.reverse
-local _lower = string.lower
-local _upper = string.upper
-local uni = false -- turns out that feature was a bad idea and let to undefined behaviours more than anything else
 
 function string.toCharArray(s)
 	local chars = {}
@@ -27,72 +20,16 @@ function string.width(...)
 	return unicode.wlen(...)
 end
 
-function string.len(str)
-	if uni then
-		return unicode.len(str)
-	else
-		return _len(str)
-	end
-end
-
-function string.rawlen(str)
-	return _len(str)
-end
-
-function string.sub(str, i, j)
-	if uni then
-		return unicode.sub(str, i, j)
-	else
-		return _sub(str, i, j)
-	end
-end
-
-function string.rawsub(str, i, j)
-	return _sub(str, i, j)
-end
-
-function string.char(...)
-	if uni then
-		return unicode.char(...)
-	else
-		return _char(...)
-	end
-end
-
-function string.reverse(str)
-	if uni then
-		return unicode.reverse(str)
-	else
-		return _reverse(str)
-	end
-end
-
-function string.rawreverse(str)
-	return _reverse(str)
-end
-
-function string.upper(str)
-	if uni then
-		return unicode.upper(str)
-	else
-		return _upper(str)
-	end
-end
-
-function string.lower(str)
-	if uni then
-		return unicode.lower(str)
-	else
-		return _lower(str)
-	end
-end
+string.rawlen = string.len
+string.rawsub = string.sub
+string.rawreverse = reverse
 
 function string.startsWith(src, s)
-	return (string.sub(src, 1, s:len()) == s)
+	return (string.sub(src, 1, #s) == s)
 end
 
 function string.endsWith(src, s)
-	return (string.sub(src, src:len()-s:len()+1, src:len()) == s)
+	return (string.sub(src, #src-#s+1, #src) == s)
 end
 
 function string.trim(s)
@@ -101,7 +38,7 @@ function string.trim(s)
 	for i=1, #s do
 		local c = ogs:sub(i, i)
 		if c == " " then
-			s = s:sub(2, #s)
+			s = s:sub(2)
 		else
 			break
 		end
@@ -112,7 +49,7 @@ function string.trim(s)
 	for i=#s, 1, -1 do
 		local c = s:sub(i, i)
 		if c == " " then
-			s = s:sub(1, #s-1)
+			s = s:sub(1)
 		else
 			break
 		end
@@ -121,12 +58,11 @@ function string.trim(s)
 end
 
 function string.split(str, sep)
-	if sep == nil then
-		sep = "%s"
-	end
-	local t={}
+	sep = sep or "%s"
+	local t,n={},0
 	for part in string.gmatch(str, "([^"..sep.."]+)") do
-		table.insert(t, part)
+		t[n+1]=part
+		n=n+1
 	end
 	return t
 end
@@ -142,48 +78,6 @@ function table.getn(table)
 	end
 	return i
 end
-
--- "Convenient" Lua extensions (deprecated)
-
-function try(func)
-	error("please report this as an issue on Fuchas's github page! try/catch unsupported")
-	local fin = function(handler)
-		handler()
-	end
-	local this = {}
-	this = {
-		catch = function(handler, filter)
-			local ok, ex = xpcall(func, function(err)
-				local exception = {
-					trace = debug.traceback(nil, 2),
-					details = err
-				}
-				return exception
-			end)
-			if not ok then
-				handler(ex)
-			end
-			this.catch = nil
-			return this
-		end,
-		finally = fin
-	}
-	return this
-end
-
-function ifOr(bool, one, two)
-	error("please report this as an issue on Fuchas's github page: ifOr unsupported")
-	return (bool and one) or two
-end
-
--- Try/Catch Example:
--- try(function()
---   print("Hello World")
--- end).catch(function(ex)
---   print("Error: " .. ex.details)
--- end).finally(function()
---   print("Function ended")
--- end)
 
 if _VERSION ~= "Lua 5.2" and not OSDATA.CONFIG["NO_52_COMPAT"] then
     load([[
